e2b02998f6fb4c9f8a4068d624f2829ec01681b5
Mohammed Naji <m.naji1512@gmail.com>
2022-03-09 17:02:58 -0800

    lec21

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function index()
     {
+
+        // if(Auth::user()->type == 'patient') {
+        //     return redirect('/');
+        // }
+
+        // dd(Auth::user());
         return view('admin.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminController;
 
-Route::prefix('admin')->name('admin.')->middleware('auth')->group(function() {
+Route::prefix('admin')->name('admin.')->middleware('auth', 'check_type')->group(function() {
     Route::get('/', [AdminController::class, 'index'])->name('index');
 });
 
@@ -18,4 +18,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-
+Route::view('/not-allowed', 'not_allowed');
